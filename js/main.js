c01d87712a7a1cf128cb647a0767f8f18d6aafa3
Ben Newman <nlb9991@gmail.com>
2021-12-21 00:26:15 -0500

    Create two-column website

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ $('#nav').affix({
 	var index=0;
 	$(document).scroll(function(){
 		var top = $('#skills').height()-$(window).scrollTop();
-		console.log(top)
 		if(top<-300){
 			if(index==0){	
 			
@@ -45,7 +44,6 @@ $('#nav').affix({
 			index++;
 		}
 	})
-	//console.log(nagativeValue)
 	});
 
 
@@ -98,3 +96,26 @@ $('#nav').affix({
 
 }
 main();
+
+// Collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].idx = coll.length - i - 1;
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+      if (this.classList.contains("active")) {
+          this.textContent = " – " // " - " // "—"
+      } else {
+          this.textContent = "tldr"
+      }
+    var content = document.getElementById(`project-description-${this.idx}`);
+    console.log(`project-description-${this.idx}`);
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
